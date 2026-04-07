3be885fa9c1253c64722ac313b33b09b1951bc92
Julia-py <juliademere18@gmail.com>
2026-04-07 08:49:25 -0300

    feat: lista de Hash

---
+++ src/HashList/TabelaHash.java
@@ -0,0 +1,49 @@
+package HashList;
+
+import java.util.ArrayList;
+
+public class TabelaHash {
+
+    private ArrayList<Aluno>[] tabela;
+    private int tamanho;
+
+
+    public TabelaHash (int tamnaho){
+        this.tamanho = tamanho;
+        tabela = new ArrayList[tamanho];
+
+        for(int i = 0; i < tamanho; i++) {
+            tabela[i] = new ArrayList <> ();
+        }
+    }
+    public int Hash(int matricula){
+        return matricula % tamanho;
+    }
+
+    public void inserirValor(int matricula, String nome){
+        int indice = Hash(matricula);
+        tabela[indice].add(new Aluno(matricula, nome));
+    }
+
+    public String pegarValor (int matricula) {
+        int indice = Hash(matricula);
+        for(Aluno aluno : tabela[indice]) {
+            if (aluno.matricula == matricula) {
+                return aluno.nome;
+            }
+        }
+        return null;
+    }
+
+    public boolean removerValor(int matricula) {
+        int indice = Hash(matricula);
+        for (int i = 0; i < tabela[indice].size(); i++) {
+            if (tabela[indice].get(i).matricula == matricula) {
+                tabela[indice].remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
